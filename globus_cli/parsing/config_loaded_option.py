b960be0e148561ab172ccbfff1daf22ba739839a
Stephen Rosen <sirosen@uchicago.edu>
2016-10-14 15:16:40 -0500

    ConfigLoadedOption gets help default from conf
    Always loads default from config, sets show_default, and turns off
    required if set. Makes helptext great.

--- globus_cli/parsing/config_loaded_option.py
+++ globus_cli/parsing/config_loaded_option.py
@@ -9,16 +9,24 @@ class ConfigLoadedOption(click.Option):
     """
     def __init__(self, *args, **kwargs):
         section, key = kwargs.pop('config_key')
-        self.session_value = lookup_option(key, section=section)
+        self.config_value = lookup_option(key, section=section)
+
+        # now set default if we got a config value
+        default = kwargs.pop('default', None)
+        # make sure self.config_value goes *first*
+        kwargs['default'] = self.config_value or default
+        if kwargs['default']:
+            kwargs['show_default'] = True
+            kwargs['required'] = False
 
         super(ConfigLoadedOption, self).__init__(*args, **kwargs)
 
     def value_is_missing(self, value):
-        if self.session_value is not None:
+        if self.config_value is not None:
             return False
         else:
             return super(ConfigLoadedOption, self).value_is_missing(value)
 
     def process_value(self, ctx, value):
-        v = self.session_value if value is None else value
+        v = self.config_value if value is None else value
         return super(ConfigLoadedOption, self).process_value(ctx, v)
